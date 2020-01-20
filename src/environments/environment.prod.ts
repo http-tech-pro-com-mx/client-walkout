b29a1dbf433370d9e34a171120fe8d1fdf5c2a5b
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2020-01-20 11:57:49 -0600

    OK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  BASE_URL: 'http://192.168.16.244:8080/ApiW'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,4 +6,5 @@
 export const environment = {
   production: false,
   BASE_URL: 'http://localhost:8080'
+
 };
